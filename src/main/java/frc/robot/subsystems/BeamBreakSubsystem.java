88125535a58c4c1cdf5d0c20c07badd6628b003a
Gatorbotics <gatorbotics1700@gmail.com>
2025-08-30 15:57:38 -0700

    beam break works! just had to change the DIO ports :/

--- src/main/java/frc/robot/subsystems/BeamBreakSubsystem.java
+++ src/main/java/frc/robot/subsystems/BeamBreakSubsystem.java
@@ -5,12 +5,11 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class BeamBreakSubsystem extends SubsystemBase {
 
-    private final int receiverPort = 1;
-    private final int transmitterPort = 0;
+    private final int receiverPort = 3;
+    private final int transmitterPort = 2;
     DigitalInput receiver = new DigitalInput(receiverPort);
     DigitalOutput transmitter = new DigitalOutput(transmitterPort);
-    
-
+    private boolean lastState;
 
     public BeamBreakSubsystem(){
         
