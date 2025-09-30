8c06733ed6505d6cb7c475fc49f7a7aa23af93fc
Mark McBride <mark@sierra.ai>
2025-09-29 22:14:06 -0700

    using a command builder to do lineups

--- src/main/java/frc/robot/commands/LineupCommand.java
+++ src/main/java/frc/robot/commands/LineupCommand.java
@@ -1,24 +1,18 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.drive.Drive;
 
 public class LineupCommand extends Command {
   private Drive drivetrainSubsystem;
 
-  public LineupCommand() {
-    
-}
+  public LineupCommand() {}
 
   @Override
-  public void execute() {
-
-  }
+  public void execute() {}
 
   @Override
   public boolean isFinished() {
-    
+    return true;
   }
 }
