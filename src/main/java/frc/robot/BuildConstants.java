ea769f47b61aaa68a43a07e2c9b8598020795b71
Violet Chaffee <26vchaffee@castilleja.org>
2025-10-02 22:10:58 -0700

    please let this fix work. pathplanner was flipping things for us but we already had them in the right coordinate system

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "preseason-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 68;
-  public static final String GIT_SHA = "ae7ba4d8b4188b9b3e4168ec5eb4af70cb928935";
-  public static final String GIT_DATE = "2025-10-02 21:25:19 EDT";
+  public static final int GIT_REVISION = 69;
+  public static final String GIT_SHA = "94e42471d050a498491de4a0f365a67d3808f78a";
+  public static final String GIT_DATE = "2025-10-02 23:31:40 EDT";
   public static final String GIT_BRANCH = "feetforward";
-  public static final String BUILD_DATE = "2025-10-02 23:03:39 EDT";
-  public static final long BUILD_UNIX_TIME = 1759460619697L;
+  public static final String BUILD_DATE = "2025-10-03 01:06:59 EDT";
+  public static final long BUILD_UNIX_TIME = 1759468019396L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -46,7 +46,6 @@ import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -166,7 +165,8 @@ public class Drive extends SubsystemBase implements Vision.VisionConsumer {
         new PPHolonomicDriveController(
             new PIDConstants(5.0, 0.0, 0.0), new PIDConstants(5.0, 0.0, 0.0)),
         PP_CONFIG,
-        () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
+        () ->
+            false, // Disable alliance flipping - tag poses are already in correct coordinate system
         this);
     Pathfinding.setPathfinder(new LocalADStarAK());
     PathPlannerLogging.setLogActivePathCallback(
