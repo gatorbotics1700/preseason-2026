6abff4bda78a6462ad3c4d226291d4bbd1d579f6
Simra Paranjpye <simraparanjpye@Simras-MacBook-Air.local>
2025-10-22 20:03:31 -0700

    coral subsystem and command beginnings of writing it

---
+++ src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+
+public class CoralShooterSubsystem extends SubsystemBase{
+    
+    
+    
+    
+    public final TalonFX topMotorLeft;
+    public final TalonFX topMotorRight;
+    private static double voltage;
+    private final DigitalInput limitSwitch;
+
+    public CoralShooterSubsystem(){
+        topMotorLeft = new TalonFX(Constants.SHOOTER_MOTOR_TOP_LEFT_CAN_ID, Constants.CANIVORE_BUS_NAME);
+        topMotorRight = new TalonFX(Constants.SHOOTER_MOTOR_TOP_RIGHT_CAN_ID, Constants.CANIVORE_BUS_NAME);
+
+        topMotorRight.getConfigurator().apply(new TalonFXConfiguration()
+            .withMotorOutput(new MotorOutputConfigs()
+                .withInverted(InvertedValue.Clockwise_Positive)))
+
+        limitSwitch = new DigitalInput(9);
+        voltage = Constants.CORAL_L4_SHOOTING_VOLTAGE;
+    }
+
+    @Override
+    public void periodic(){
+
+    }
+
+    public void setMotorVoltage(double voltage){
+        topMotorLeft.setVoltage(voltage);
+        topMotorRight.setVoltage(voltage);
+    }
+
+    public boolean getLimitSwitch(){
+        return limitSwitch.get();
+    }
+
+    
+}
