461aa8715b0d884c8dfd64360600e6ce1b75cec8
gatorbotics <gatorbotics1700@gmail.com>
2025-11-13 19:51:15 -0800

    worked on stop drive command and troubleshooted AK SG SA AR KF

--- src/main/java/frc/robot/commands/DistanceSensorCommand.java
+++ src/main/java/frc/robot/commands/DistanceSensorCommand.java
@@ -1,16 +1,30 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
+
+import java.lang.module.ModuleDescriptor.Requires;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
 import frc.robot.subsystems.DistanceSensor;
 
-public class DistanceSensorCommand {
-    private Subsystem distanceSensor = new DistanceSensor()
-    .addRequirements(distanceSensor);
+public class DistanceSensorCommand extends Command {
+    private DistanceSensor distanceSensor;
+
+    public  DistanceSensorCommand(DistanceSensor distanceSensor1){
+        distanceSensor = distanceSensor1;
+        //addRequirements(distanceSensor);
+    }
+
 }
 
+@Override
 public boolean isFinished(){
-    if(((DistanceSensor) distanceSensor).getDistance() < 0.9){
-
+    if((distanceSensor).getDistance() < 0.9){
+    
     }
 }
+
+
 
