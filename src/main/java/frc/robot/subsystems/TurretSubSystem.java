cb5c7a846c0626579f1e3db459b78db8cb1d4586
Doris <29dduan@castilleja.org>
2025-11-14 17:50:20 -0800

    added turret code

---
+++ src/main/java/frc/robot/subsystems/TurretSubSystem.java
@@ -0,0 +1,37 @@
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycle;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.generated.TunerConstants;
+
+public class TurretSubSystem extends SubsystemBase {
+  private final TalonFX turretMotor;
+
+  private static DutyCycleOut dutyCycleOut = new DutyCycleOut(0);
+
+  public TurretSubSystem() {
+    turretMotor =
+        new TalonFX(Constants.TURRET_MOTOR_CAN_ID, TunerConstants.kCANBus);              
+  }
+
+  public void setMotorVoltage(double voltage) {
+    turretMotor.setVoltage(voltage);
+  }
+
+  public double getTurretDegrees(){
+    return (turretMotor.getPosition().getValueAsDouble() / Constants.TURRET_GEAR_RATIO * 360) % 360;
+    
+  }
+  public void setSpeed(double speed){
+    turretMotor.setControl(dutyCycleOut.withOutput(speed));
+  }
+  
+  }
