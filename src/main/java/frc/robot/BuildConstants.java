8ac1e34101f34cc83ed70460c909fc6557cd7916
gatorbotics <gatorbotics1700@gmail.com>
2025-12-10 15:53:50 -0800

    got it speedy! and it works!

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "preseason-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 109;
-  public static final String GIT_SHA = "2d509b9d2556af301bffe809bb9645828a0d1a5c";
-  public static final String GIT_DATE = "2025-12-10 16:32:51 EST";
+  public static final int GIT_REVISION = 110;
+  public static final String GIT_SHA = "70fb476a0bc743591142e544fa9f62d81fd41031";
+  public static final String GIT_DATE = "2025-12-10 17:01:12 EST";
   public static final String GIT_BRANCH = "autoRotate";
-  public static final String BUILD_DATE = "2025-12-10 16:48:14 EST";
-  public static final long BUILD_UNIX_TIME = 1765403294360L;
+  public static final String BUILD_DATE = "2025-12-10 18:48:14 EST";
+  public static final long BUILD_UNIX_TIME = 1765410494078L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -162,10 +162,10 @@ public class DriveCommands {
     // Create PID controller for automatic rotation control (only used when desiredAngle is set)
     ProfiledPIDController angleController =
         new ProfiledPIDController(
-            ANGLE_KP,
+            ANGLE_KP * 9,
             0.0,
             ANGLE_KD,
-            new TrapezoidProfile.Constraints(ANGLE_MAX_VELOCITY, ANGLE_MAX_ACCELERATION));
+            new TrapezoidProfile.Constraints(ANGLE_MAX_VELOCITY * 9, ANGLE_MAX_ACCELERATION * 9));
     angleController.enableContinuousInput(-Math.PI, Math.PI);
 
     Rotation2d[] lastDesiredAngle = {null};
